a3656b6ba73e2082c436500b34363e7c0064fda4
Divya raj Lavti <drlraj2805@outlook.com>
2020-07-15 13:07:32 +0530

    final

--- mail.py
+++ mail.py
@@ -2,9 +2,9 @@ import smtplib, ssl
 
 port = 465   					#For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "drlraj9116@gmail.com"  		# Enter your address
+sender_email = "draj2223@gmail.com"  		# Enter your address
 password = 'monu1616' 			# Enter password
-receiver_email = "draj2223@gmail.com"  	# Enter receiver address
+receiver_email = "drlraj9116@gmail.com"  	# Enter receiver address
 
 message = """\
 Subject:  Model successfully trained
