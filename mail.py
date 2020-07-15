5fb220d73abbcb3938b3fb56654bbfd8551f3b07
Divya raj Lavti <drlraj2805@outlook.com>
2020-07-15 13:05:54 +0530

    updated

--- mail.py
+++ mail.py
@@ -1,12 +1,16 @@
-import smtplib
-rcvr = "drlraj9116@gmail.com"
-sender = "drlraj2805@gmail.com"
-msg = "Hey Developer, your model has greater than 96% accuracy and is ready to use for predictions!!" 
-pswd = "***" #password for sender's mail address
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.ehlo()
-server.starttls()
-server.login(sender,pswd)
-print("Access Granted")
-server.sendmail(sender,rcvr,msg)
-print("Mail sent!!")
+import smtplib, ssl
+
+port = 465   					#For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "drlraj9116@gmail.com"  		# Enter your address
+password = 'monu1616' 			# Enter password
+receiver_email = "draj2223@gmail.com"  	# Enter receiver address
+
+message = """\
+Subject:  Model successfully trained
+Congratulations accuracy achieved."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
